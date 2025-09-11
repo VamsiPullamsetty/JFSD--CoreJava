84abb24d2d02797f35c08e59986098069e60bc9d
VamsiPullamsetty <vamsipullamsetty@gmail.com>
2025-09-11 18:39:45 +0530

    VariableDemo

---
+++ VariableDemo.java
@@ -0,0 +1,13 @@
+public class VariableDemo {
+    public static void main(String[] args)
+    {
+        String name = "Vamsi";
+        int  id = 98;
+        double cgpa = 8.4;
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(cgpa);
+    }
+    
+}
+
